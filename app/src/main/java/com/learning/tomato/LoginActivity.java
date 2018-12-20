86f67751c48197a10436837b3684fd98f78427e7
JustDoItSW <592813685@qq.com>
2018-12-20 12:41:47 +0800

    布局按百分比来分布，画三角形与矩形以及设置圆角、渐变色等参数，登录按钮添加点击事件

--- app/src/main/java/com/learning/tomato/LoginActivity.java
+++ app/src/main/java/com/learning/tomato/LoginActivity.java
@@ -2,15 +2,25 @@ package com.learning.tomato;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends BaseActivity {
     private static final String TAG = "LoginActivity";
+    public String uri="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Log.d(TAG,"onCreate execute");
+        Button login_button=findViewById(R.id.login);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.actionStart(LoginActivity.this,uri);
+            }
+        });
     }
 }
--- app/src/main/java/com/learning/tomato/MainActivity.java
+++ app/src/main/java/com/learning/tomato/MainActivity.java
@@ -1,5 +1,7 @@
 package com.learning.tomato;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends BaseActivity {
@@ -8,5 +10,11 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void actionStart(Context context,String uri){
+        Intent intent=new Intent(context,MainActivity.class);
+        intent.putExtra("uri",uri);
+        context.startActivity(intent);
     }
 }
